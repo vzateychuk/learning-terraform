bda9d32a4ec98c7993cc9f59fc456e91c64a2d55
vzateychuk <vzateychuk@gmail.com>
2023-07-20 23:57:20 +0300

    aws_instance + gitignore

--- main.tf
+++ main.tf
@@ -15,9 +15,12 @@ data "aws_ami" "app_ami" {
 }
 
 resource "aws_instance" "web" {
+
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+
   tags = {
-    Name = "HelloWorld"
+    Name = "web"
+    Env = "dev"
   }
 }
